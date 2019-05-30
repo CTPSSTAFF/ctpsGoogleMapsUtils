bca345bf5c7f57f8e790a02dfe72194c11ff32a9
Ben Krepp <bkrepp@ctps.org>
2019-05-30 10:51:02 -0400

    Added support for drawing dotted/dashed lines.

--- ctpsGoogleMapsUtils.js
+++ ctpsGoogleMapsUtils.js
@@ -2,21 +2,40 @@
 // ===> N.B. This library *requires* that the Google Maps JS API library is loaded before it!
 //
 // Author: B. Krepp
-// Date: March 12, 2019
+// Date:    March 12, 2019
+// Revised: May 30, 2019
 (function() {
     // Internals of the library (if any) go here
     //
 	// *** Public API of the library begins here. ***
 	return ctpsGoogleMapsUtils = {
-		version : "1.0",
+		version : "2.0",
         // function: drawPolylineFeature
         // parameters: 1. polylineFeature (GeoJSON)
         //             2. GoogleMaps map object
         //             3. object containing styling info
+        //             4. dotted (or null)
+        //
+        // If the 'dotted' parm is passed, and its value is true, this indicates that
+        // a 'dotted/dashed' line is to be rendered. 
+        // Note that when rendering a 'dotted/dashed' line, the 'strokeOpacity' property of
+        // the style object will be set to 0, regardless of the value passed in!
+        //
+        // For information on rendering dotted/dashed' lines with the Google Maps API see:
+        //      https://developers.google.com/maps/documentation/javascript/examples/overlay-symbol-dashed
+        // 
         // return value: the GoogleMaps polyline feature added to the map
-        drawPolylineFeature : function(lineFeature, gMap, style) {
+        drawPolylineFeature : function(lineFeature, gMap, style, dotted) {
             var gmPolyline = {}, aFeatCoords = [], point = {}, aAllPoints = [];
+            var gmPolylineOpts = {};
             var i, j;
+            // The following is used when rendering dotted/dashed lines: 
+            // Define a symbol using SVG path notation, with an opacity of 1.
+            var lineSymbol = {
+                    path: 'M 0,-1 0,1',
+                    strokeOpacity: 1,
+                    scale: 4
+                };
             if (lineFeature.geometry.type === 'MultiLineString') {
                 // console.log('Rendering MultiLintString feature.');
                 aFeatCoords = lineFeature.geometry.coordinates;
@@ -28,11 +47,17 @@
                         point = new google.maps.LatLng({ 'lat': aCoord[1], 'lng': aCoord[0] });
                         aAllPoints.push(point);
                     } // for j in aFeatCoords[i]
-                    gmPolyline = new google.maps.Polyline({ path            : aAllPoints,
-                                                            map             : gMap,
-                                                            strokeColor     : style.strokeColor,
-                                                            strokeOpacity   : style.strokeOpacity,
-                                                            strokeWeight    : style.strokeWeight });
+                    gmPolylineOpts = { path             : aAllPoints,
+                                       map              : gMap,
+                                       strokeColor      : style.strokeColor,
+                                       strokeOpacity    : style.strokeOpacity,
+                                       strokeWeight     : style.strokeWeight };   
+                    if (arguments.length === 4 && dotted === true) {
+                        // Render 'dotted/dashed' line
+                        gmPolylineOpts.strokeOpacity = 0;
+                        gmPolylineOpts.icons = [{ icon: lineSymbol, offset: '0', repeat: '20px' }];                       
+                    }
+                    gmPolyline = new google.maps.Polyline(gmPolylineOpts);
                 } // for i in aFeatureCoords.length
             } else if (lineFeature.geometry.type === 'LineString') {
                 // console.log('Rendering LineString feature.');
@@ -42,11 +67,17 @@
                     point = new google.maps.LatLng({ 'lat': aCoord[1], 'lng': aCoord[0] });
                     aAllPoints.push(point);
                 } // for i in aFeatureCoords.length
-                gmPolyline = new google.maps.Polyline({ path            : aAllPoints,
-                                                        map             : gMap,
-                                                        strokeColor     : style.strokeColor,
-                                                        strokeOpacity   : style.strokeOpacity,
-                                                        strokeWeight    : style.strokeWeight });
+                gmPolylineOpts = { path             : aAllPoints,
+                                   map              : gMap,
+                                   strokeColor      : style.strokeColor,
+                                   strokeOpacity    : style.strokeOpacity,
+                                   strokeWeight     : style.strokeWeight };  
+                 if (arguments.length === 4 && dotted === true) {
+                    // Render 'dotted/dashed' line
+                    gmPolylineOpts.strokeOpacity = 0;
+                    gmPolylineOpts.icons = [{ icon: lineSymbol, offset: '0', repeat: '20px' }];                       
+                }                                 
+                gmPolyline = new google.maps.Polyline(gmPolylineOpts);
             } else {
                 console.log('Feature has unrecognized geometry type: ' + lineFeature.geometry.type); 
             }
